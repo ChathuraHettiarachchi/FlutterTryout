d3267030090e1d6daa95f68cc16fe58abd686071
Chathura Hettiarachchi <it13091408@my.sliit.lk>
2021-04-06 18:31:00 +0530

    covid tutorial with UI and api

---
+++ 07. Covid-19 UI Try/android/app/src/main/kotlin/com/choota/covid_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.choota.covid_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 08. Covid-19-App with API/android/app/src/main/kotlin/com/choota/covid_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.choota.covid_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
